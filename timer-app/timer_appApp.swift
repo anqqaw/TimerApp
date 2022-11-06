209ceab8fc61652704f3b9d05e2cc0299da89f15
Aarni Klemetti <aarni.klemetti@gmail.com>
2022-11-06 23:06:43 +0200

    Initial Commit

---
+++ timer-app/timer_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  timer_appApp.swift
+//  timer-app
+//
+//  Created by Aarni on 6.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct timer_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
